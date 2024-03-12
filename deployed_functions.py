a70f43130193d8e80d7d6374e706ceb5c823429a
nadimakk <nadimakkaoui1@gmail.com>
2024-03-12 22:18:56 +0200

    implement semantic search functions, upload FAISS indices to volume

--- deployed_functions.py
+++ deployed_functions.py
@@ -1,5 +1,16 @@
 from modal import Stub, Volume, Image
 import os
+from enum import Enum
+
+class FaissIndex(Enum):
+    ChunkSize_512_ChunkOverlap_50 = {
+        'size': 512,
+        'overlap': 50,   
+    }
+    ChunkSize_1500_ChunkOverlap_300 = {
+        'size': 1500,
+        'overlap': 300,   
+    }
 
 stub = Stub("philosophy-question-answerer")
 
@@ -10,10 +21,21 @@ image_download_model = (
     .pip_install("requests")
 )
 
+image_download_embedding_model = (
+    Image.debian_slim(python_version="3.10")
+    .pip_install("transformers", "torch")
+)
+
 image_query_model = (
     Image.debian_slim(python_version="3.10")
     .pip_install("ctransformers", "torch")
 )
+
+image_semantic_search = (
+    Image.debian_slim(python_version="3.10")
+    .pip_install("langchain", "sentence-transformers", "faiss-cpu", "torch", "torchvision")
+)
+
 
 @stub.function(volumes={"/data": vol}, image=image_download_model)
 def download_model_to_volume(model_file_name, download_url):
@@ -34,7 +56,7 @@ def download_model_to_volume(model_file_name, download_url):
 
         print('Model successfully downloaded.')
 
-        print(f'Saving to volume.')
+        print('Saving to volume.')
 
         with open(file_path, 'wb') as file:
             file.write(response.content)
@@ -43,6 +65,32 @@ def download_model_to_volume(model_file_name, download_url):
 
     else:
         print(f"Failed to download the model file. Status code: {response.status_code}")
+
+    print(f'Committing changes to volume.')
+    vol.commit()
+    print('Changes committed.')
+
+
+@stub.function(volumes={"/data": vol}, image=image_download_embedding_model)
+def download_embedding_model_to_volume(embedding_model_name):
+
+    from transformers import AutoTokenizer, AutoModel
+
+    save_directory = '/data/embedding_model/'
+
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+        print(f"Created directory {save_directory}")
+
+    print("Downloading embedding model...")
+    tokenizer = AutoTokenizer.from_pretrained(embedding_model_name)
+    model = AutoModel.from_pretrained(embedding_model_name)
+    print("Embedding model downloaded.")
+
+    print("Saving to volume.")
+    tokenizer.save_pretrained(save_directory)
+    model.save_pretrained(save_directory)
+    print(f"Embedding model have been saved to {save_directory}")
 
     print(f'Committing changes to volume.')
     vol.commit()
@@ -81,3 +129,45 @@ def query_mistral_7b_instruct_v0p2(prompt):
     response = stub.registered_functions['query_model'].local(model_file_name, prompt)
 
     return response
+
+
+@stub.function(volumes={"/data": vol}, image=image_semantic_search)
+def semantic_search(question, faiss_index_type, num_matched_excerpts):
+
+    from langchain_community.vectorstores import FAISS
+
+    vol.reload()
+
+    chunk_size = faiss_index_type.value['size']
+    chunk_overlap = faiss_index_type.value['overlap']
+    faiss_index_path = f'/data/faiss_indices/ChunkSize_{chunk_size}_ChunkOverlap_{chunk_overlap}'
+
+    embedding_model = stub.registered_functions['load_embedding_model'].local()
+
+    faiss_index = FAISS.load_local(faiss_index_path,
+                                   embedding_model,
+                                    allow_dangerous_deserialization=True)
+
+    results = faiss_index.similarity_search(query=question, k=num_matched_excerpts)
+    documents = [
+        {
+            'text': result.page_content,
+            'source': result.metadata['source']
+        } for result in results
+    ]
+
+    return documents
+
+
+@stub.function()
+def load_embedding_model():
+
+    from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+
+    embedding_model = HuggingFaceEmbeddings(
+        model_name='/data/embedding_model/',
+        model_kwargs={'device': 'cpu'},
+        encode_kwargs={'device': 'cpu', 'batch_size': 10}
+    )
+
+    return embedding_model
--- invocations.py
+++ invocations.py
@@ -1,14 +1,28 @@
 import modal
 from dotenv import load_dotenv
+from deployed_functions import FaissIndex
 
 load_dotenv()
+
 
 # download_model_to_volume = modal.Function.lookup("philosophy-question-answerer", "download_model_to_volume")
 # model_file_name = "mistral-7b-instruct-v0.2.Q5_K_M.gguf"
 # download_url = 'https://cdn-lfs-us-1.huggingface.co/repos/72/62/726219e98582d16c24a66629a4dec1b0761b91c918e15dea2625b4293c134a92/b85cdd596ddd76f3194047b9108a73c74d77ba04bef49255a50fc0cfbda83d32?response-content-disposition=attachment%3B+filename*%3DUTF-8%27%27mistral-7b-instruct-v0.2.Q5_K_M.gguf%3B+filename%3D%22mistral-7b-instruct-v0.2.Q5_K_M.gguf%22%3B&Expires=1710072370&Policy=eyJTdGF0ZW1lbnQiOlt7IkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTcxMDA3MjM3MH19LCJSZXNvdXJjZSI6Imh0dHBzOi8vY2RuLWxmcy11cy0xLmh1Z2dpbmdmYWNlLmNvL3JlcG9zLzcyLzYyLzcyNjIxOWU5ODU4MmQxNmMyNGE2NjYyOWE0ZGVjMWIwNzYxYjkxYzkxOGUxNWRlYTI2MjViNDI5M2MxMzRhOTIvYjg1Y2RkNTk2ZGRkNzZmMzE5NDA0N2I5MTA4YTczYzc0ZDc3YmEwNGJlZjQ5MjU1YTUwZmMwY2ZiZGE4M2QzMj9yZXNwb25zZS1jb250ZW50LWRpc3Bvc2l0aW9uPSoifV19&Signature=E2yFU8%7EaGjUaK9OjaSOCH%7EyDo6Aram-YOhpZaB30UMRXqNa-tq7E4TgKi17WWOfOHJ9C3xmsitixVL6%7EsZd274EX4GbzEoVte6kFgmAjeF4RBXcmhHLjGAErXnsDHlByfUpnrBe5GxU-pme3zTqCHuxnkDlnqKpYTDMachkMS6X2DE5k7Qe-OwQLY1XAjsP0sZO36zGRg7Sxa%7E64R48bBycG4YI5PLEcIgkcsgPuhEL2-rABu2JZkuAhGOW%7El0DGKffjidiLBCSSvWl2VanwyiAiwz1W-VnFRNoY8Uwbf7Y%7E21mhDx3qnDuilWpCeqqgJeXpt3YbTfWHCWFCM0bJ6Q__&Key-Pair-Id=KCD77M1F0VK2B'
 # download_model_to_volume.remote(model_file_name, download_url)
 
-query_mistral_7b_instruct_v0p2 = modal.Function.lookup("philosophy-question-answerer", "query_mistral_7b_instruct_v0p2")
-prompt = 'Why is the sky blue?'
-response = query_mistral_7b_instruct_v0p2.remote(prompt)
-print(response)
+# download_embedding_model_to_volume = modal.Function.lookup("philosophy-question-answerer", "download_embedding_model_to_volume")
+# embedding_model_name = 'sentence-transformers/all-MiniLM-L6-v2'
+# download_embedding_model_to_volume.remote(embedding_model_name)
+
+# query_mistral_7b_instruct_v0p2 = modal.Function.lookup("philosophy-question-answerer", "query_mistral_7b_instruct_v0p2")
+# print(query_mistral_7b_instruct_v0p2)
+# prompt = 'Why is the sky blue?'
+# response = query_mistral_7b_instruct_v0p2.remote(prompt)
+# print(response)
+
+semantic_search = modal.Function.lookup("philosophy-question-answerer", "semantic_search")
+question = "What is Wittgenstein's private language argument?"
+faiss_index_type = FaissIndex.ChunkSize_512_ChunkOverlap_50
+num_matched_excerpts = 2
+documents = semantic_search.remote(question, faiss_index_type, num_matched_excerpts)
+print(documents)
